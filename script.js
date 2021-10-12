e478a929c0d07fc4d58486cfa7cadaadfe49eca4
RianVitor26 <rianvitorlhe@gmail.com>
2021-10-11 22:50:36 -0300

    Deixando os starbucks funcional

---
+++ script.js
@@ -0,0 +1,27 @@
+const btnStarbuckGreen = document.querySelector('.btn-starbuck-green')
+const btnStarbuckPink = document.querySelector('.btn-starbuck-pink')
+const btnStarbuckPurple = document.querySelector('.btn-starbuck-purple')
+const starbuckGreen = document.querySelector('.starbuck-green')
+const starbuckPink = document.querySelector('.starbuck-pink')
+const starbuckPurple = document.querySelector('.starbuck-purple')
+
+function AlternarPurple() {
+    starbuckPurple.classList.add("active")
+    starbuckPink.classList.remove("active")
+    starbuckGreen.classList.remove("active")
+}
+btnStarbuckPurple.addEventListener("click", AlternarPurple)
+
+function AlternarPink() {
+    starbuckPurple.classList.remove("active")
+    starbuckPink.classList.add("active")
+    starbuckGreen.classList.remove("active")
+}
+btnStarbuckPink.addEventListener("click", AlternarPink)
+
+function AlternarGreen() {
+    starbuckPurple.classList.remove("active")
+    starbuckPink.classList.remove("active")
+    starbuckGreen.classList.add("active")
+}
+btnStarbuckGreen.addEventListener("click", AlternarGreen)
